b64f97e30d6a67bf1ce7b7362cbe25d82026fa3a
ivanvn19 <ivan.vazquezneira@gmail.com>
2023-06-14 14:52:31 -0300

    LoteEntreRios

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ingreso",
+      "name": "Ingreso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7284389547316881,
+        "pitch": 0.1482774292703528,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2657768950687789,
+          "pitch": 0.12747964662328748,
+          "rotation": 0,
+          "target": "1-calle-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-calle-i",
+      "name": "Calle I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7249784365961993,
+        "pitch": 0.13825865558563244,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1306687756362273,
+          "pitch": 0.10497747302409621,
+          "rotation": 0,
+          "target": "0-ingreso"
+        },
+        {
+          "yaw": 1.5099583872007196,
+          "pitch": 0.18989871808792635,
+          "rotation": 0,
+          "target": "4-lote-i"
+        },
+        {
+          "yaw": -0.041215955582963204,
+          "pitch": 0.0058862574405189605,
+          "rotation": 0,
+          "target": "5-calle-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-lote-iii",
+      "name": "Lote III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.649876859273924,
+          "pitch": 0.05860808340018053,
+          "rotation": 0,
+          "target": "7-calle-iv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lote-ii",
+      "name": "Lote II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.099919281652502,
+        "pitch": 0.1850010905235262,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2029305357641924,
+          "pitch": 0.07446214438805754,
+          "rotation": 0,
+          "target": "6-calle-iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-lote-i",
+      "name": "Lote I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.2999913544590047,
+        "pitch": 0.24318327216683677,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.89610758895056,
+          "pitch": 0.17302320716552622,
+          "rotation": 0,
+          "target": "1-calle-i"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-calle-ii",
+      "name": "Calle II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.140784915705405,
+          "pitch": 0.07907251655510805,
+          "rotation": 0,
+          "target": "1-calle-i"
+        },
+        {
+          "yaw": -0.037303909389748924,
+          "pitch": 0.05244764472053021,
+          "rotation": 0,
+          "target": "6-calle-iii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-calle-iii",
+      "name": "Calle III",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8396395279523272,
+          "pitch": 0.1805843857432663,
+          "rotation": 0,
+          "target": "3-lote-ii"
+        },
+        {
+          "yaw": -2.9776003328348484,
+          "pitch": 0.0826393358270785,
+          "rotation": 0,
+          "target": "5-calle-ii"
+        },
+        {
+          "yaw": 0.1383659103755388,
+          "pitch": 0.008790173815143021,
+          "rotation": 0,
+          "target": "7-calle-iv"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-calle-iv",
+      "name": "Calle IV",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3759468360271754,
+          "pitch": 0.12267814892181406,
+          "rotation": 0.7853981633974483,
+          "target": "2-lote-iii"
+        },
+        {
+          "yaw": 3.135866642130809,
+          "pitch": 0.13977591015840574,
+          "rotation": 0,
+          "target": "6-calle-iii"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lotes Entre Ríos ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
